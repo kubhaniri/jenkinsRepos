82542edfb1935522e8a3a702c5a0ed6d99229cbf
kubhaniri <kubhaniri@gmail.com>
2020-05-01 22:48:00 +0200

    Calculator implement

--- src/main/java/lib/Calculator.java
+++ src/main/java/lib/Calculator.java
@@ -1,4 +1,8 @@
 package lib;
 
 public class Calculator {
+
+    public int add(int a){
+        return 10 + a;
+    }
 }
---
+++ src/test/java/lib/CalculatorTest.java
@@ -0,0 +1,13 @@
+package lib;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testAdd() {
+    }
+}
