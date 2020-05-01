e661e394e509f59d7844afc336d6fd937460e9a4
kubhaniri <kubhaniri@gmail.com>
2020-05-01 19:19:43 +0200

    Fixing Test and setting my private maven repo

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -21,6 +22,6 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue01()
     {
-        assertTrue( false );
+        assertFalse( false );
     }
 }
